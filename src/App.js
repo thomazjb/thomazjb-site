a0f28b74764008541932f8a8ec24eccc6cedbd99
Thomaz J. Boncompagni <thomazjb@hotmail.com>
2022-06-08 10:03:11 -0300

    linkedin

--- src/App.js
+++ src/App.js
@@ -5,19 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-      <pre>     .----.   @   @
-   / .-"-.`.  \v/
-   | | '\ \ \_/ )
- ,-\ `-.' /.'  /
-'---`----'----'</pre>
         <p>
-        <pre>  ________  ______  __  ______ _____          __   ____ 
- /_  __/ / / / __ \/  |/  /   /__  /         / /  / __ )
-  / / / /_/ / / / / /|_/ / /| | / /     __  / / / __  |
- / / / __  / /_/ / /  / / ___ |/ /__   / /_/ / / /_/ / 
-/_/ /_/ /_/\____/_/  /_/_/  |_/____/   \____(_)_____(_)
-                                                       </pre>
+         THOMAZ J. BONCOMPAGNI
         </p>
+        <a
+          className="App-link"
+          href="https://www.linkedin.com/in/thomazjb/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Linked-in
+        </a>
       </header>
     </div>
   );
