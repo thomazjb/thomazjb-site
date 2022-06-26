53276efc85f8fe99049c72c8020f423b0e0ce8f0
Thomaz J. Boncompagni <thomaz.jb@hotmail.com>
2022-06-26 00:12:57 -0300

    updating pages

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,7 +9,8 @@ function Home() {
         <img className="rounded-full" src="https://pbs.twimg.com/profile_images/1058535016903385088/yf4L1WUq_400x400.jpg" alt="Eu" width="150px" ></img>
         </div>
         <br></br>
-        <p>SOBRE</p>
+        <h1><strong>Sobre</strong></h1>
+        <p>Em construção</p>
         </Card>
       </div>
     
