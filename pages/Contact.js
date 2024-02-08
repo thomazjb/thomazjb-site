0eb16abf5cf145f3ad62e41cc5f103d949ef6046
Thomaz Juliann Boncompagni <thomaz.jb@hotmail.com>
2024-02-08 17:11:49 -0300

    Update Contact.js

--- pages/Contact.js
+++ pages/Contact.js
@@ -6,7 +6,7 @@ function Contact() {
       <div className="App-body">
       <Card>
         <div >
-        <img className="rounded-full" src="https://pbs.twimg.com/profile_images/1058535016903385088/yf4L1WUq_400x400.jpg" alt="Eu" width="150px" ></img>
+        <img className="rounded-full" src="https://media.licdn.com/dms/image/D4D03AQHZtnA81JI1tQ/profile-displayphoto-shrink_800_800/0/1704312640496?e=1712793600&v=beta&t=IlPrbCE1ELu_zB_hFklmGuXCC_GG435Y3A1m0rVaj9M" alt="Eu" width="150px" ></img>
         </div>
         <br></br>
         <h1><strong>Contato</strong></h1>
