c132dbcd04793ab657df1382f98d9908af223f30
Thomaz Juliann Boncompagni <thomaz.jb@hotmail.com>
2024-02-08 17:51:38 -0300

    improving about

--- src/pages/About.js
+++ src/pages/About.js
@@ -1,12 +1,12 @@
 import React from "react";
 import Card from "../Components/Card";
 
-function Home() {
+function About() {
   return (
       <div className="App-body">
       <Card>
         <div >
-        <img className="rounded-full" src="https://media.licdn.com/dms/image/D4D03AQFObt-Qp31jqQ/profile-displayphoto-shrink_800_800/0/1683832275386?e=1708560000&v=beta&t=HRyIWdAcmsEN31Ktp-EPJhHSoIxEXvvHCuwjQhFCWTg" alt="Eu" width="150px" ></img>
+        <img className="rounded-full" style={{imageRendering: "pixelated", border: "3px dashed"}} src="https://media.licdn.com/dms/image/D4D03AQHZtnA81JI1tQ/profile-displayphoto-shrink_800_800/0/1704312640496?e=1712793600&v=beta&t=IlPrbCE1ELu_zB_hFklmGuXCC_GG435Y3A1m0rVaj9M" alt="Eu" width="150px" ></img>
         </div>
         <br></br>
         <h1><strong>Sobre</strong></h1>
@@ -17,4 +17,4 @@ function Home() {
   );
 }
 
-export default Home;
+export default About;
