8141c492ec688a1daf93e6bbf4f3ccda02976937
Thomaz Juliann Boncompagni <thomaz.jb@hotmail.com>
2024-10-02 20:49:50 -0300

    Update Home.js

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -8,7 +8,7 @@ function Home() {
       <div className="App-body scale-up-center">
       <Card>
         <div >
-        <img className="rounded-full" style={{imageRendering: "pixelated", border: "3px dashed"}} src="https://instagram.fbfh10-1.fna.fbcdn.net/v/t51.2885-19/449711657_496974046177040_6133834477489105397_n.jpg?stp=dst-jpg_s150x150&_nc_ht=instagram.fbfh10-1.fna.fbcdn.net&_nc_cat=108&_nc_ohc=-eQ9w6UZWogQ7kNvgGA2xIw&_nc_gid=284b7ebb6d7b47fb94b27ed0eaeab735&edm=AP4sbd4BAAAA&ccb=7-5&oh=00_AYBzBbA_nB3yRSrJrNKEyguBJX8ajDDuGZcye4zhf6iASA&oe=6703B65B&_nc_sid=7a9f4b" alt="Eu" width="150px" ></img>
+        <img className="rounded-full" style={{imageRendering: "pixelated", border: "3px dashed"}} src="https://images2.imgbox.com/1c/a8/uU8mmm2y_o.jpg" alt="Eu" width="150px" ></img>
         </div>
         <br></br>
         <p><strong>THOMAZ J. BONCOMPAGNI</strong></p>
