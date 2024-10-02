f74bcc5b01a3e28a4b9eb80f66e07552b57f5e49
Thomaz Juliann Boncompagni <thomaz.jb@hotmail.com>
2024-10-02 20:54:27 -0300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -6,7 +6,7 @@ function About() {
       <div className="App-body">
       <Card>
         <div >
-        <img className="rounded-full" style={{imageRendering: "pixelated", border: "3px dashed"}} src="https://media.licdn.com/dms/image/D4D03AQHZtnA81JI1tQ/profile-displayphoto-shrink_800_800/0/1704312640496?e=1712793600&v=beta&t=IlPrbCE1ELu_zB_hFklmGuXCC_GG435Y3A1m0rVaj9M" alt="Eu" width="150px" ></img>
+        <img className="rounded-full" style={{imageRendering: "pixelated", border: "3px dashed"}} src="https://thumbs2.imgbox.com/1c/a8/uU8mmm2y_t.jpg" alt="Eu" width="150px" ></img>
         </div>
         <br></br>
         <h1><strong>Sobre</strong></h1>
